e43aed754e1837dfa6db5b2b1dea214e99778db4
CodeInPolish <marcin.krasowski@hotmail.fr>
2019-05-10 15:19:55 +0200

    Fix: read & write (when reaching EOF)

--- fileIO.c
+++ fileIO.c
@@ -4,10 +4,12 @@
 void receiveAndWrite(char* path, int socketFd, int bufferSize){
 	int readChar;
 	char* buffer = SYSN(malloc(bufferSize*sizeof(char)), "malloc error");
-	int fileFd = SYS(open(path, O_WRONLY|O_CREAT), "open error");
-	while((readChar = read(socketFd, buffer, bufferSize))!= 0){
+	int fileFd = SYS(open(path, O_WRONLY|O_CREAT|O_TRUNC), "open error");
+	while((readChar = read(socketFd, buffer, bufferSize)) > 0){
 		write(fileFd, buffer, readChar);
+		memset(buffer, 0, bufferSize);
 	}
+	write(fileFd, buffer, strlen(buffer));
 	SYS(fchmod(fileFd, S_IRUSR|S_IWUSR|S_IXUSR),"set permission error");
 	free(buffer);
 	close(fileFd);
@@ -17,12 +19,11 @@ void readAndSendFile(char* path, int socketFd, int bufferSize){
 	int fileFd = open(path, O_RDONLY);
 	int readChar;
 	char* buffer = SYSN(malloc(bufferSize*sizeof(char)), "malloc error");
-	while((readChar = read(fileFd, buffer, bufferSize)) != -1){
+	while((readChar = read(fileFd, buffer, bufferSize)) > 0){
 		send(socketFd, buffer, readChar, 0);
-		if(buffer[readChar] == '\0'){
-			break;
-		}
+		memset(buffer, 0, bufferSize);
 	}
+	send(socketFd, buffer, strlen(buffer), 0);
 	free(buffer);
 	close(fileFd);
 }
